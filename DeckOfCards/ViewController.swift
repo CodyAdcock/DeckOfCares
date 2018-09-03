53dcb73e7152636f4c4fadf3b9e6293d85c869ee
Cody Adcock <codyadcock10@gmail.com>
2018-09-03 10:13:11 -0600

    First Commit

---
+++ DeckOfCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeckOfCards
+//
+//  Created by Cody on 9/3/18.
+//  Copyright © 2018 Cody Adcock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
